30e7c0f5bfb94f0a4bc2abc597e107209c688780
risa <risairma21@gmail.com>
2023-06-01 23:18:44 +0700

    feat: add test on CarController

---
+++ app/controllers/car.spec.js
@@ -0,0 +1,22 @@
+const dayjs = require('dayjs');
+const  CarController  = require('./CarController');
+
+const { Car, UserCar } = require('../models');
+
+const carModel = Car;
+const userCarModel = UserCar;
+const carController = new CarController({ carModel, userCarModel, dayjs });
+
+describe('Get list query from request', () => {
+    const mockReq = {
+        query: {
+            size: 10,
+            availableAt: new Date(),
+        },
+    };
+
+    it('it must return object from inputted query from request', () => {
+        let response = carController.getListQueryFromRequest(mockReq);
+        expect(response).toBeDefined();
+    });
+});
